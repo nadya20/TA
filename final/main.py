a604954c47912c6fa28751f5680a7571b47049bb
Muhammad Alif Akbar <alif.akbar@bukalapak.com>
2018-11-03 16:26:43 +0700

    regions

--- final/main.py
+++ final/main.py
@@ -6,6 +6,18 @@ import os
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 
+# region state
+lecture_name = "Alif"
+lecture_nip = "1000111100"
+lecture_course = "CS012"
+
+student_nim = "1103132163"
+student_course = "CS012"
+student_presence = "90%"
+
+total_student = "10"
+# endregion
+
 # window setup
 window = tk.Tk()
 window.title("TA Nadya")
@@ -14,6 +26,15 @@ window.resizable(0,0)
 
 def switch(page):
     page.lift()
+
+# region pages
+# page main
+page_main = tk.Frame(window)
+img = ImageTk.PhotoImage(Image.open(file_dir+"/logo_telu.jpg"))
+tk.Label(page_main, image=img).pack()
+tk.Label(page_main, text="Silahkan Masukkan Kartu Anda").pack()
+tk.Button(page_main, command=lambda: switch(page_submit), text="Kehadiran").pack()
+page_main.place(in_=window, x=0, y=0, relwidth=1, relheight=1)
 
 # failed page
 page_failed = tk.Frame(window)
@@ -29,32 +50,25 @@ page_success.place(in_=window, x=0, y=0, relwidth=1, relheight=1)
 
 # lecture page
 page_lecture = tk.Frame(window)
-tk.Label(page_lecture, text="Nama Dosen: Alif").pack()
-tk.Label(page_lecture, text="NIP: 1000111100").pack()
-tk.Label(page_lecture, text="Kode Mata Kuliah: CS012").pack()
+tk.Label(page_lecture, text="Nama Dosen: " + lecture_name).pack()
+tk.Label(page_lecture, text="NIP: " + lecture_nip).pack()
+tk.Label(page_lecture, text="Kode Mata Kuliah: " + lecture_course).pack()
 page_lecture.place(in_=window, x=0, y=0, relwidth=1, relheight=1)
 
 # student page
 page_student = tk.Frame(window)
-tk.Label(page_student, text="NIM: 1103132163").pack()
-tk.Label(page_student, text="Kode Mata Kuliah: CS012").pack()
-tk.Label(page_student, text="Presensi: 90%").pack()
+tk.Label(page_student, text="NIM: " + student_nim).pack()
+tk.Label(page_student, text="Kode Mata Kuliah: " + student_course).pack()
+tk.Label(page_student, text="Presensi: " + student_presence).pack()
 page_student.place(in_=window, x=0, y=0, relwidth=1, relheight=1)
 
 # submit page
 page_submit = tk.Frame(window)
-tk.Label(page_submit, text="Kode Mata Kuliah: CS012").pack()
-tk.Label(page_submit, text="Jumlah Mahasiswa: 10").pack()
+tk.Label(page_submit, text="Kode Mata Kuliah: " + lecture_course).pack()
+tk.Label(page_submit, text="Jumlah Mahasiswa: " + total_student).pack()
 tk.Button(page_submit, command=lambda: switch(page_main), text="Kehadiran").pack()
 page_submit.place(in_=window, x=0, y=0, relwidth=1, relheight=1)
-
-# page main
-page_main = tk.Frame(window)
-img = ImageTk.PhotoImage(Image.open(file_dir+"/logo_telu.jpg"))
-tk.Label(page_main, image=img).pack()
-tk.Label(page_main, text="Silahkan Masukkan Kartu Anda").pack()
-tk.Button(page_main, command=lambda: switch(page_submit), text="Kehadiran").pack()
-page_main.place(in_=window, x=0, y=0, relwidth=1, relheight=1)
+# endregion
 
 # main loop
 switch(page_main)
