c6ad91c8d71df75c8cbf7fc1c7209e2909703aeb
nadya20 <nadyajung@gmail.com>
2018-11-09 21:12:31 +0700

    block

--- final/main.py
+++ final/main.py
@@ -96,6 +96,7 @@ class App(object):
 
     def check_for_state_change(self, first_time = False):
         print self.state
+        time_to_wait = 0
         if not first_time:
             if self.state.wait_for == LECTURE and self.state.lecture is not None:
                 self.switch(self.page_success)
@@ -103,25 +104,30 @@ class App(object):
                 self.window.after(5000, lambda: self.switch(self.page_lecture))
                 self.window.after(10000, lambda: self.switch(self.page_main))
                 self.state.wait_for = STUDENT
+                time_to_wait += 15000
 
             elif self.state.wait_for == LECTURE and self.state.lecture is None:
                 self.switch(self.page_failed)
                 self.window.after(5000, lambda: self.switch(self.page_main))
+                time_to_wait += 5000
 
             elif self.state.wait_for == STUDENT and self.state.student is not None:
                 self.switch(self.page_success)
                 self.page_student.setData(self.state.student.id, self.state.student.courses, self.state.student_course_index)
                 self.window.after(5000, lambda: self.switch(self.page_student))
                 self.window.after(10000, lambda: self.switch(self.page_main))
+                time_to_wait += 15000
 
             elif self.state.wait_for == STUDENT and self.state.student is None:
                 self.switch(self.page_failed)
                 self.window.after(5000, lambda: self.switch(self.page_main))
                 self.state.student = None
+                time_to_wait += 5000
 
             if self.state.lecture is not None:
                 self.page_submit.setData(self.state.lecture.subject, self.state.total_student)
 
+        time.sleep(time_to_wait) # block current thread until page finish reload
         self.worker = threading.Thread(target=self.process_card)
         self.worker.start()
 
